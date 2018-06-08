b882978ab2956896c7e8c7a03682cd71d165b739
shindigira <shindigira@gmail.com>
2018-06-07 20:30:57 -0400

    start finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { inject, observer } from "mobx-react";
 
 class App extends Component {
+  clearComplete = () => {};
+  createNew = e => {};
+  filter = e => {};
+  list = () => {};
   render() {
     return (
       <div className="App">
@@ -15,6 +21,17 @@ class App extends Component {
         </p>
       </div>
     );
+    // return (
+    //   <div>
+    //     <h1>List of things</h1>
+    //     <input className="new" onKeyPress={this.createNew} />
+    //     <input className="filter" value={"filter"} onChange={this.filter} />
+    //     <ul>{this.list()}</ul>
+    //     <a href="#" onClick={this.clearComplete}>
+    //       Clear Complete
+    //     </a>
+    //   </div>
+    // );
   }
 }
 
---
+++ src/stores/store1.js
@@ -0,0 +1,9 @@
+import { observable, computed, action } from "mobx";
+
+class store1 {
+  createItem = item => {};
+
+  clearComplete = () => {};
+}
+
+export default new store1();
