fd97464501f3aa758080515cd39663b39270bf53
shindigira <shindigira@gmail.com>
2018-06-07 23:08:48 -0400

    start2

--- src/App.js
+++ src/App.js
@@ -7,31 +7,47 @@ import { inject, observer } from "mobx-react";
 class App extends Component {
   clearComplete = () => {};
   createNew = e => {};
-  filter = e => {};
-  list = () => {};
+  onFilter = e => {};
+  // list = itemArray => {
+  //   return itemArray.map(item => (
+  //     <li key={item.id}>
+  //       <input
+  //         type="checkbox"
+  //         onChange={this.toggleComplete(item)}
+  //         value={item.complete}
+  //         checked={item.complete}
+  //       />
+  //       <span>{item.value}</span>
+  //     </li>
+  //   ));
+  // };
+  toggleComplete = item => {};
   render() {
+    //   return (
+    //     <div className="App">
+    //       <header className="App-header">
+    //         <img src={logo} className="App-logo" alt="logo" />
+    //         <h1 className="App-title">Welcome to React</h1>
+    //       </header>
+    //       <p className="App-intro">
+    //         To get started, edit <code>src/App.js</code> and save to reload.
+    //       </p>
+    //     </div>
+    //   );
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>List of things</h1>
+        <input className="new" onKeyPress={this.createNew} />
+        <input
+          className="filter"
+          value={"this.props.filter"}
+          onChange={this.onFilter}
+        />
+        {}
+        {/* <ul>{this.list()}</ul> */}
+        <button onClick={this.clearComplete}>Clear Complete</button>
       </div>
     );
-    // return (
-    //   <div>
-    //     <h1>List of things</h1>
-    //     <input className="new" onKeyPress={this.createNew} />
-    //     <input className="filter" value={"filter"} onChange={this.filter} />
-    //     <ul>{this.list()}</ul>
-    //     <a href="#" onClick={this.clearComplete}>
-    //       Clear Complete
-    //     </a>
-    //   </div>
-    // );
   }
 }
 
--- src/stores/store1.js
+++ src/stores/store1.js
@@ -1,9 +1,8 @@
 import { observable, computed, action } from "mobx";
 
 class store1 {
-  createItem = item => {};
-
-  clearComplete = () => {};
+  // createItem = item => {};
+  // clearComplete = () => {};
 }
 
 export default new store1();
